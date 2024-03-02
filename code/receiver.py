02ae508bbfde738a83e3fb0d14744312ffb4f428
Anthony Bradburn <ajbradburn@gmail.com>
2024-03-02 07:18:35 -0500

    I have a working proof-of-concept for the remote control.         There is a bug I've identified. Something about initializing I2C, and then assigning a variable afterwards causes issues with I2S output. Adding a simple print statement afterwards solves the problem.
            Right now, the variable is calling a servo object from a 3rd
    party library, but that isn't important. making my own function, and
    calling that function will produce the same result.

--- code/receiver.py
+++ code/receiver.py
@@ -7,14 +7,14 @@ print(network.WLAN().config('mac'))
 print(ubinascii.hexlify(network.WLAN().config('mac'),':').decode())
 
 from machine import I2C, Pin, I2S, SDCard
-import servo
+from servo import Servos
 import time
-#from uos import mount
-import uos
+from uos import mount, umount
 from os import listdir
 from time import sleep
 
-
+'''
+'''
 # A WLAN interface must be active to send()/recv()
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
@@ -26,7 +26,8 @@ e.active(True)
 # Start Servo Config
 # Connect to the PCA9685 servo driver.
 i2c = I2C(0, scl=3, sda=8)
-ser = servo.Servos(i2c, address=0x40)
+ser = Servos(i2c, address=0x40)
+print(ser) # CRITICALLY IMPORTANT. This print statement enables I2S to work properly. Some combination of the i2c assignment, and then a variable assignemtn afterwards makes the I2S sound terrible. Simply printing the variable resolves the issue...
 # End Servo
 
 # Start Motor Config
@@ -35,7 +36,6 @@ mf = Pin(9, Pin.OUT) # Motor Forward
 mb = Pin(10, Pin.OUT) # Motor Backward
 # End Motor
 
-'''
 # Start Sound Config
 # Configure the I2S connection to the Max98357A audio amplifier.
 #sample_rate_in_hz = 44100
@@ -58,67 +58,6 @@ audio_out = I2S(
 sd = SDCard(slot=3, sck=Pin(14), mosi=Pin(13), miso=Pin(12), cs=Pin(15))
 mount(sd, "/sd")
 #print(os.listdir("/sd"))
-
-####
-# Open the file and seek data.
-wav_file = '/sd/{}'.format('snake.wav')
-wav = open(wav_file,'rb')
-pos = wav.seek(44) 
-
-# Allocate audio buffer.
-#   memoryview used to reduce heap allocation in while loop
-wav_samples = bytearray(1024)
-wav_samples_mv = memoryview(wav_samples)
-audio_out.shift
-
-# Play Audio File
-while True:
-    num_read = wav.readinto(wav_samples_mv)
-    if num_read == 0:
-        break
-    num_written = 0
-    # Increase the volume.
-    #I2S.shift(buf=wav_samples_mv, bits=16, shift=1)
-    while num_written < num_read:
-        num_written += audio_out.write(wav_samples_mv[num_written:num_read])
-wav.close()
-'''
-bck_pin = Pin(5)                                                                                                                                             
-ws_pin = Pin(6)  
-sdout_pin = Pin(4)
-
-# channelformat settings:
-#     mono WAV:  channelformat=I2S.ONLY_LEFT
-audio_out = I2S(
-    1, 
-    sck=bck_pin, ws=ws_pin, sd=sdout_pin, 
-    mode=I2S.TX,
-    bits=16,
-    format=I2S.MONO, 
-    rate=44100,
-    ibuf=1024
-    )
-
-sd = SDCard(slot=3, sck=Pin(14), mosi=Pin(13), miso=Pin(12), cs=Pin(15))
-uos.mount(sd, "/sd")
-
-wav_file = '/sd/{}'.format('snake.wav')
-wav = open(wav_file,'rb')
-pos = wav.seek(44) 
-wav_samples = bytearray(1024)
-wav_samples_mv = memoryview(wav_samples)
-audio_out.shift
-
-while True:
-    num_read = wav.readinto(wav_samples_mv)
-    if num_read == 0:
-        break
-    num_written = 0
-    # Increase the volume.
-    #I2S.shift(buf=wav_samples_mv, bits=16, shift=1)
-    while num_written < num_read:
-        num_written += audio_out.write(wav_samples_mv[num_written:num_read])
-####
 
 def blink():
     ser.position(0, 180)
